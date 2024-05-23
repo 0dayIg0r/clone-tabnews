e5ef42948a529432ee7809ee5096aeba129668cf
Igor M <90478654+0dayIg0r@users.noreply.github.com>
2024-05-23 01:32:08 +0000

    add `database.js` and `.env` file

---
+++ pages/api/v1/status/index.js
@@ -0,0 +1,10 @@
+import database from '../../../../infra/database'
+
+async function status(req, res) {
+    const result = await database.query('SELECT 1 + 1;')
+    console.log(result)
+  
+    res.status(200).json({ ok: 'ok' })
+  }
+
+  export default status
